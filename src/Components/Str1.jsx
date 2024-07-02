07021ba0c8333bf36c13c9fca42ad68f0840b81e
AlinaGerman <151366227+AlinaGerman@users.noreply.github.com>
2024-07-02 20:38:36 +0700

    Update Str1.jsx

--- src/Components/Str1.jsx
+++ src/Components/Str1.jsx
@@ -1,10 +1,10 @@
-//import StartDiv from "./StartDiv/StartDiv"
-//import Reasons from "./Reasons/Reasons.jsx"
-//import Info from "./Info/Info.jsx"
-//import Entertaiment from "./Entertaiment/Entertaiment.jsx"
-//import Events from "./Events/Events.jsx"
+import StartDiv from "./StartDiv/StartDiv"
+import Reasons from "./Reasons/Reasons.jsx"
+import Info from "./Info/Info.jsx"
+import Entertaiment from "./Entertaiment/Entertaiment.jsx"
+import Events from "./Events/Events.jsx"
 import Emotions from "./Emotions/Emotions.jsx"
-//import Questions from "./Questions/Questions.jsx"
+import Questions from "./Questions/Questions.jsx"
 import MulCarouselDiv from "./MultyCarousel/MulCarouselDiv.jsx"
 
 import { dataStarPeople } from "./MultyCarousel/dataStarPeople";
@@ -13,18 +13,18 @@ import { dataStarPeople } from "./MultyCarousel/dataStarPeople";
 export default function Str1(){
     return(
         <>
-            {/*<StartDiv str1={true} style={{marginBottom:'10%'}}></StartDiv>
+        <StartDiv str1={true} style={{marginBottom:'10%'}}></StartDiv>
         <Reasons></Reasons>
         <Info></Info>
-        <Entertaiment></Entertaiment>*/}
+        <Entertaiment></Entertaiment>
         <Emotions str1={true} buttonName='КУПИТЬ БИЛЕТ В ПАРК'>Неподдельные эмоции наших гостей</Emotions>
-            {/*<Events></Events>*/}
+        <Events></Events>
         <MulCarouselDiv type='StarPeople' style={{marginBottom:'1%'}} buttonClass='buyTicket' buyTicketButton={true}
             id='ticketToPark' buttonName='КУПИТЬ БИЛЕТ В ПАРК' displayElement4={true} dataComp={dataStarPeople} widthImg='100%'>Впечатления звёздных гостей</MulCarouselDiv>
-            {/*<MulCarouselDiv type='Comments' style={{marginBottom:'5%'}}>Отзывы</MulCarouselDiv>
+        <MulCarouselDiv type='Comments' style={{marginBottom:'5%'}}>Отзывы</MulCarouselDiv>
         <MulCarouselDiv type='Sales' buttonClass='allSales' buttonName='ВСЕ АКЦИИ' style={{marginBottom:'5%'}} id='sales'>Акции</MulCarouselDiv>
         <Questions></Questions>
-        <MulCarouselDiv type='News'  buttonClass='allSales' buttonName='ВСЕ НОВОСТИ' style={{marginBottom:'5%'}}>Новости</MulCarouselDiv>*/}
+        <MulCarouselDiv type='News'  buttonClass='allSales' buttonName='ВСЕ НОВОСТИ' style={{marginBottom:'5%'}}>Новости</MulCarouselDiv>
         </>
     )
 }
