4dfa289037668fa5eada69afbc2e2a5cfa66a6de
AlinaGerman <151366227+AlinaGerman@users.noreply.github.com>
2024-07-03 13:00:44 +0700

    Update links.js

--- src/Components/Header/links.js
+++ src/Components/Header/links.js
@@ -1,53 +1,68 @@
+import h1 from './imagesHoliday/h1.svg'
+import h2 from './imagesHoliday/h2.svg'
+import h3 from './imagesHoliday/h3.svg'
+import h4 from './imagesHoliday/h4.svg'
+
 export var links = [
     {
         id: 'entertainment',
         title: 'АРЕНА',
-        url:'#'
+        url:'#entertaiment',
+        class:'EntertLinkArena'
     },
     {
         id: 'entertainment',
         title: 'КУБ',
-        url:'#'
+        url:'#entertaiment',
+        class:'EntertLinkKub'
     },
     {
         id: 'entertainment',
         title: 'АТТРАКЦИОНЫ',
-        url:'#'
+        url:'#entertaiment',
+        class:'EntertLinkAttraction'
     },
     {
         id: 'entertainment',
         title: 'СТЕНДЫ',
-        url:'#'
+        url:'#entertaiment',
+        class:'EntertLinkStend'
     },
     {
         id: 'entertainment',
         title: 'PVP-СОРЕВНОВАНИЯ',
-        url:'#'
+        url:'#entertaiment',
+        class:'EntertLinkPVP'
     },
     {
         id: 'entertainment',
         title: 'БИЛЕТ В ПАРК',
-        url:'#'
+        url:'#ticketToPark',
+        class:'EntertLinkTicket'
     },
     {
         id:'holidays',
         title:'ДЕТСКИЕ ДНИ РОЖДЕНИЯ',
-        url:'#'
+        url:'#event',
+        icon:h1
     },
     {
         id:'holidays',
         title:'ВЗРОСЛЫЕ ДНИ РОЖДЕНИЯ',
-        url:'#'
+        url:'#event',
+        icon:h2
     },
     {
         id:'holidays',
         title:'КОРПОРАТИВЫ',
-        url:'#'
+        url:'#event',
+        icon:h3
     },
     {
         id:'holidays',
         title:'НЕСТАНДРТНОЕ МЕРОПРИЯТИЕ',
-        url:'#'
+        url:'#event',
+        icon:h4
     },
     {
         id:'place',
